7a94499ca5200d5fcdcf5087149125007654e31d
nov matake <nov@matake.jp>
2012-09-14 19:16:59 +0900

    remove GIT for now

--- lib/account_chooser.rb
+++ lib/account_chooser.rb
@@ -3,11 +3,13 @@ module AccountChooser
     module_function
 
     def load(options = {})
+      config = options.inject('') do |config, (key, value)|
+        config << "window.accountchooser.config.#{key} = #{value.to_json};\n"
+      end
       script = <<-SCRIPT
-      <script type="text/javascript" src="https://www.accountchooser.com/ac.js">
-        #{options.to_json[1..-2]}
-      </script>
+      <script type="text/javascript" src="https://www.accountchooser.com/ac.js"></script>
       <script>
+        #{config}
         window.accountchooser.cdshelper.USER_STATUS_TIMEOUT_ = 5000;
       </script>
       SCRIPT
